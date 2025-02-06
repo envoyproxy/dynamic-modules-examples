3f8d992cfcaaaa349caf1d016836e824074c3a10
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-02-06 11:11:16 -0800

    more comments
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,13 +6,15 @@ use passthrough::*;
 declare_init_functions!(init, new_http_filter_config_fn);
 
 /// This implements the [`envoy_proxy_dynamic_modules_rust_sdk::ProgramInitFunction`].
+///
+/// This is called exactly once when the module is loaded.
 fn init() -> bool {
     true
 }
 
 /// This implements the [`envoy_proxy_dynamic_modules_rust_sdk::NewHttpFilterConfigFunction`].
 ///
-/// This is the entrypoint when the module is specified in the HTTP filter chain.
+/// This is the entrypoint every time a new HTTP filter is created via the DynamicModuleFilter config.
 ///
 /// Each argument matches the corresponding argument in the Envoy config here:
 /// https://storage.googleapis.com/envoy-pr/be95c85/docs/api-v3/extensions/filters/http/dynamic_modules/v3/dynamic_modules.proto.html#envoy-v3-api-msg-extensions-filters-http-dynamic-modules-v3-dynamicmodulefilter
